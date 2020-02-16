b0014a403eae84fb6e0a2a0a5dd02252b2389970
Julian Phillips Parker <Julian.park1988@gmail.com>
2020-02-15 21:14:35 -0500

    added optional stuff

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -10,57 +10,63 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var incorrectButtonThree: UIButton!
+    @IBOutlet weak var incorrectButtonTwo: UIButton!
+    @IBOutlet weak var incorrectButtonOne: UIButton!
+    @IBOutlet weak var correctButton: UIButton!
+    @IBOutlet weak var cardView: UIView!
     var cardHidden = true;
     @IBOutlet weak var backLabel: UILabel!
     @IBOutlet weak var frontLabel: UILabel!
     @IBOutlet weak var answerToggle: UISwitch!
     override func viewDidLoad() {
         super.viewDidLoad()
-            updateUI()
+        cardView.layer.cornerRadius = 20.0
+        cardView.clipsToBounds = true
+        cardView.layer.shadowRadius = 15.0
+        cardView.layer.shadowOpacity = 0.2
+        
+        correctButton.layer.borderWidth = 3.0
+        correctButton.layer.borderColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        incorrectButtonOne.layer.borderWidth = 3.0
+        incorrectButtonOne.layer.borderColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        incorrectButtonTwo.layer.borderWidth = 3.0
+        incorrectButtonTwo.layer.borderColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        incorrectButtonThree.layer.borderWidth = 3.0
+        incorrectButtonThree.layer.borderColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        
+        incorrectButtonOne.layer.cornerRadius = 20.0
+        incorrectButtonTwo.layer.cornerRadius = 20.0
+        incorrectButtonThree.layer.cornerRadius = 20.0
+        correctButton.layer.cornerRadius = 20.0
+        incorrectButtonOne.clipsToBounds = true
+        incorrectButtonTwo.clipsToBounds = true
+        incorrectButtonThree.clipsToBounds = true
+        correctButton.clipsToBounds = true
+        updateUI()
     }
+    
     @IBAction func didButtonTap(_ sender: UIButton) {
         answerToggle.isHidden = false
         
-        if sender.currentTitle == "Julian Parker"{
             /* TODO
-             -Make Button Highlight Green Color
-             -Add 'Vertical' Shake Animation
-             -Drop Opacity of other options
+             - Add 'Vertical' Shake Animation on correct button
+             - Add 'Horizontal' Shake Animation on incorrect button
              - Make other options unclickable
              */
+        
+            incorrectButtonOne.layer.borderColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
+            incorrectButtonTwo.layer.borderColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
+            incorrectButtonThree.layer.borderColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
+            correctButton.layer.borderColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
+            incorrectButtonOne.alpha = 0.5
+            incorrectButtonTwo.alpha = 0.5
+            incorrectButtonThree.alpha = 0.5
             
-        }
-        else
-            if sender.currentTitle == "Tavis Thompson"{
-                /*
-                 TODO
-                 
-                 -Make Button Highlight Red Color
-                 -Add 'horizontal'  Shake Animation
-                 
-                 */
-        }
-        else
-            if sender.currentTitle == "Cameron Thomas"{
-                /* TODO
-                 
-                    -Make Button Highlight Red Color
-                    -Add 'horizontal'  Shake Animation
-                */
-
-        }
-        else
-            if sender.currentTitle == "Bryson Hill"{
-             /* TODO
-                 
-                    -Make Button Highlight Red Color
-                    -Add 'horizontal'  Shake Animation
-             */
-
-        }
+            
+            
+       
          
-        /* TODO
-         - add logic to stop user from being able to tap button after first tap */
         
         updateCard()
         updateUI()
@@ -79,7 +85,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func didTapOnFlash(_ sender: Any) {
-       
+       answerToggle.isHidden = false
        updateCard()
        updateUI()
     }
